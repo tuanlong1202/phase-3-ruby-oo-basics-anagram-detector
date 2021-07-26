b0fe16d9e0fa373e6767fdf511c3c0be34c2686b
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-26 17:49:32 -0400

    Phase 3 ruby oo basics anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(a_word)
+        @word = a_word
+    end
+    def match(arrWords)
+        arrRet = []
+        arrWords.each do |item|
+            if sameCompose(@word,item)
+                arrRet << item
+            end
+        end
+        arrRet
+    end
+    def sameCompose(firstWord,secondWord)
+        arrTmp1 = firstWord.split("")
+        arrTmp2 = secondWord.split("")
+        arrTmp1.sort == arrTmp2.sort
+    end
+end
